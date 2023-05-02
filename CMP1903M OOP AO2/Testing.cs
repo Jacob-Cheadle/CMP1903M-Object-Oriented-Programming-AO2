8855c2beb46c0f3890fd4cc2ce272485fc2f9c2b
Jacob-Cheadle <117683892+Jacob-Cheadle@users.noreply.github.com>
2023-05-02 22:33:40 +0100

    Added basic menu functionality

--- CMP1903M OOP AO2/Testing.cs
+++ CMP1903M OOP AO2/Testing.cs
@@ -4,7 +4,54 @@
     {
         public static void Test()
         {
-            Console.WriteLine("Science Bitch");
+            string Choice = "";
+
+            Start();
+            while ((Choice != "1") & (Choice != "2") & (Choice != "3"))
+            {
+                DisplayMenu();
+                Choice = Console.ReadLine();
+                if (Choice == "1")
+                { }
+                else if (Choice == "2")
+                { }
+                else if (Choice == "3")
+                { }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Invalid Input - Please Try Again!");
+                }
+            }
         }
+
+        public static void Start()
+        {
+            Console.WriteLine("Welcome to MathsTutor!");
+        }
+
+        public static void Quit()
+        {
+            Console.WriteLine("Thanks For Using MathsTutor!");
+        }
+
+        public static void DisplayMenu()
+        {
+            Console.WriteLine("Options: ");
+            Console.WriteLine("1) Instructions");
+            Console.WriteLine("2) Deal 3 Cards");
+            Console.WriteLine("3) Quit");
+
+            Console.Write("Choice: ");
+        }
+
+        public static void DisplayInstructions()
+        {
+            Console.WriteLine("Instructions: ");
+            Console.WriteLine("-Select the 'Deal 3 Cards' Option From the Menu");
+            Console.WriteLine("-A Mathematical Equation Will Be Displayed, Guess The Correct Answer");
+            Console.WriteLine("-If Your Answer Is Correct You Will Be Told, Else You Will Be Corrected");
+        }
+
     }
 }
