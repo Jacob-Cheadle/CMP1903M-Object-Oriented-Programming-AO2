db52e38cb1de9c68dec6b4d129b389ca7c0c7077
Jacob-Cheadle <117683892+Jacob-Cheadle@users.noreply.github.com>
2023-05-02 14:24:22 +0100

    Testing Pog actually made it exist so yaknow pretty cool ig

---
+++ CMP1903M OOP AO2/Testing.cs
@@ -0,0 +1,10 @@
+﻿namespace CMP1903M_OOP_AO2
+{
+    internal class Testing
+    {
+        public static void Test()
+        {
+            Console.WriteLine("Science Bitch");
+        }
+    }
+}
