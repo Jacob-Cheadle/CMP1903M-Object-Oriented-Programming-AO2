5e0a8ea3f797378d5a22ccd5f9735ce6286fa5f2
Jacob-Cheadle <117683892+Jacob-Cheadle@users.noreply.github.com>
2023-05-03 10:40:20 +0100

    Implemented the Pack and Card classes from the last assessment

---
+++ CMP1903M OOP AO2/Card.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_OOP_AO2
+{
+    class Card
+    {
+        public int Suit { get; set; } //Card pnly has 2 variables, Value and Suit
+        public int Value { get; set; }
+        public Card(int Suit, int Value) //constructor to set Suit and Value (encapsulation)
+        {
+            this.Suit = Suit;
+            this.Value = Value;
+        }
+
+    }
+}
